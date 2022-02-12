c5d94b3dc1551081e21b937577e48ecf65a4f044
sonhw <ths1611@naver.com>
2022-02-12 19:24:30 +0900

    0212

--- now_price.py
+++ now_price.py
@@ -2,6 +2,7 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5 import uic
+from PyQt5.QtCore import *
 import pykorbit
 
 form_class = uic.loadUiType("now_price.ui")[0]
@@ -14,6 +15,10 @@ class MyWindow(QMainWindow, form_class):
         
         self.setWindowTitle('GoMar_beta_ver_1')
         self.setWindowIcon(QIcon('bitcoin_icon.png'))
+        
+        self.timer = QTimer(self)
+        self.timer.start(1000)
+        self.timer.timeout.connect(self.timeout)
     
         
     def btn1_clicked(self):
@@ -21,6 +26,11 @@ class MyWindow(QMainWindow, form_class):
         price = pykorbit.get_current_price("BTC")
         print(price)
         self.lineEdit.setText(str(price))
+        
+    def timeout(self):
+        cur_time = QTime.currentTime()
+        str_time = cur_time.toString('hh:mm:ss')
+        self.statusBar().showMesseage(str_time)
  
 app = QApplication(sys.argv) # QApplication클래스에서 이벤트루프 설정할 메소드 호출할 수 있다.
 window = MyWindow()
